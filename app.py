e0cc28d4ec1027f375a01169f5890045ca3f544f
louayamor <amor.louay20@fmail.com>
2024-12-17 15:18:54 +0100

    Removed debug for prod

--- app.py
+++ app.py
@@ -95,4 +95,5 @@ def predict():
         return render_template("index.html")
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080, debug=True)
+    # app.run(host="0.0.0.0", port=8080, debug=True)
+    app.run(host="0.0.0.0", port=8080)
